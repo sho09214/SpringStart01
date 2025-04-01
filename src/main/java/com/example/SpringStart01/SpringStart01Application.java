66db220aa241ab50f0b2a21fc0c53af5d72dfa0a
sho09214 <motohashi.sho@gmail.com>
2025-04-01 22:09:22 +0900

    create project

---
+++ src/main/java/com/example/SpringStart01/SpringStart01Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringStart01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStart01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStart01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringStart01/SpringStart01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringStart01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStart01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
