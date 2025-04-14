00c25b397e4c2576f949457ba7e635c895ad752c
sho09214 <motohashi.sho@gmail.com>
2025-04-14 22:18:58 +0900

    Add - 前回講義分のソース 作成

---
+++ src/main/java/com/example/SpringStart01/form/ReviewRegistForm.java
@@ -0,0 +1,27 @@
+package com.example.SpringStart01.form;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+import java.sql.Date;
+
+@Data
+public class ReviewRegistForm {
+    @NotNull(message = "入力してください")
+    @Min(value = 1, message = "正の整数を入力してください。")
+    private Integer restaurantId;
+
+    @Size(min = 4, max = 16, message = "4文字から16文字で指定してください。")
+    private String userId;
+
+    @Past(message = "今日以前の日付を入力してください。")
+    private Date visitDate;
+
+    @NotNull(message = "入力してください。")
+    @Min(value = 1, message = "1-5で指定してください。")
+    @Max(value = 5, message = "1-5で指定してください。")
+    private Integer rating;
+
+    @Size(min = 1, max = 128, message = "1文字から128文字で指定してください。")
+    private String comment;
+}
